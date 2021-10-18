8ab9e2f77dc3b7463a5b011e603c54c0b1b99165
Jakub Dziopa <jd@main.lan>
2021-10-18 23:27:31 +0200

    doklepywanie warsztatów

--- 22Rysie3/syg.cpp
+++ 22Rysie3/syg.cpp
@@ -11,63 +11,69 @@
 #define pb push_back
 using namespace std;
 
-priority_queue<pair<int,int>>pq;
+priority_queue<pair<int, int>> pq;
 bool in_queue[10001];
-vector<pair<int,int>>G[10001],S;
-int costs[10001];
-
+vector<pair<int, int>> G[10001], S;
+int costs[10001], timea[10001], timet[10001];
 
 int main()
 {
-    int n,m,x,y,a,b,c,msc,odl,timer=1;
-    fill(costs,costs+10001,INT_MAX);
+    int n, m, x, y, a, b, c, msc, odl, timer = 1;
+    fill(costs, costs + 10001, INT_MAX);
+    fill(timet, timet + 10001, INT_MAX);
 
-    scanf("%d%d%d%d",&x,&y,&n,&m);
-    
+    scanf("%d%d%d%d", &x, &y, &n, &m);
 
     for (int i = 0; i < m; i++)
     {
-        scanf("%d%d%d",&a,&b,&c);
-        
-        G[a].pb({b,c});
-        G[b].pb({a,c});
+        scanf("%d%d%d", &a, &b, &c);
+
+        G[a].pb({b, c});
+        G[b].pb({a, c});
     }
-    S.pb({0,0});
-    for(int i=0 ; i<n ; ++i)
+    S.pb({0, 0});
+    for (int i = 0; i < n; ++i)
     {
-        scanf("%d%d",&a,&b);
-        S.pb({a,b});
+        scanf("%d%d", &a, &b);
+        S.pb({a, b});
     }
 
-    pq.push({0,x});
-    in_queue[x]=1;
-    int cooldown;
-    while(!pq.empty())
+    pq.push({0, x});
+    in_queue[x] = 1;
+    costs[x] = 0;
+    timet[x] = 0;
+    while (!pq.empty())
     {
-        msc=pq.top().second;
-        odl=pq.top().first;
+        msc = pq.top().second;
+        odl = -pq.top().first;
         pq.pop();
 
-
-
-        for(int i=0 ; i<G[msc].size() ; ++i)
+        for (int i = 0; i < G[msc].size(); ++i)
         {
-            if(!in_queue[G[msc][i].first])
+
+            timet[G[msc][i].first] = min(timet[G[msc][i].first], G[msc][i].second + timet[msc]);
+            if ((timet[G[msc][i].first]) % (S[G[msc][i].first].first + S[G[msc][i].first].second) >= S[G[msc][i].first].first && G[msc][i].first != y)
             {
-                pq.push(G[msc][i]);in_queue[G[msc][i].first]=1;
+
+                timet[G[msc][i].first] += S[G[msc][i].first].first + S[G[msc][i].first].second - ((timet[G[msc][i].first]) % (S[G[msc][i].first].first + S[G[msc][i].first].second));
             }
 
-
+            if (!in_queue[G[msc][i].first])
+            {
+                pq.push({-(G[msc][i].second + timet[msc]), G[msc][i].first});
+                in_queue[G[msc][i].first] = 1;
+            }
+            if (costs[G[msc][i].first] > costs[msc] + G[msc][i].second + timet[msc])
+            {
+                costs[G[msc][i].first] = costs[msc] + G[msc][i].second + timet[msc];
+            }
         }
-
-
     }
 
-    
-
-
-
-
+    if (timet[y] == INT_MAX)
+        printf("-1");
+    else
+        printf("%d", timet[y]);
 
     return 0;
 }
---
+++ 3JakubDziopa/ptr.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a,b;
+    float offst;
+
+    scanf("%d%d",&a,&b);
+
+    if(a!=0&&b!=0){}
+    else 
+    {
+        if(a==b){printf("125.00 125.00");return 0;}
+
+        if(a>0)
+        {
+            offst=a/125;
+
+
+        }
+
+
+    }
+
+
+
+
+    return 0;
+}
